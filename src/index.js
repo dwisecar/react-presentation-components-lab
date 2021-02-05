ae0c4da154afa64978c81b846b8a848e1fc8ccec
dwisecar <dwisecar@gmail.com>
2021-02-05 09:20:18 -0800

    done

--- src/index.js
+++ src/index.js
@@ -4,10 +4,14 @@ import ReactDOM from 'react-dom';
 import SimpleComponent from './components/SimpleComponent';
 import SimplerComponent from './components/SimplerComponent';
 
+function logFun(){
+  console.log('fun')
+}
+
 ReactDOM.render(
   <div>
     <SimpleComponent />
-    <SimplerComponent handleClick={undefined} />
+    <SimplerComponent handleClick={logFun} />
   </div>,
   document.getElementById('root')
 );
